df9856f1da0277de8c608e0a9fdce6250a2d1c39
Balázs Indig <indba@digitus.itk.ppke.hu>
2019-10-16 18:37:34 +0200

    Enable REST API frontend through xtsv

--- hunspellpyrest.py
+++ hunspellpyrest.py
@@ -2,16 +2,26 @@
 # -*- coding: utf-8, vim: expandtab:ts=4 -*-
 
 from xtsv import pipeline_rest_api, singleton_store_factory
-from hunspellpy import HunspellPy
 
-hunspell_spell = (HunspellPy, (), {'task': 'spell', 'source_fields': {'form'}, 'target_fields': ['spell']})
-hunspell_stem = (HunspellPy, (), {'task': 'stem', 'source_fields': {'form'}, 'target_fields': ['anas']})
-hunspell_analyze = (HunspellPy, (), {'task': 'analyze', 'source_fields': {'form'}, 'target_fields': ['anas']})
-hunspell_dstem = (HunspellPy, (), {'task': 'dstem', 'source_fields': {'form'}, 'target_fields': ['spell', 'anas']})
 
-tools = {'spell': hunspell_spell, 'stem': hunspell_stem, 'analyze': hunspell_analyze, 'dstem': hunspell_dstem}
+hunspell_spell = ('hunspellpy', 'HunspellPy', 'spell',
+                  (), {'task': 'spell', 'source_fields': {'form'}, 'target_fields': ['spell']})
+hunspell_stem = ('hunspellpy', 'HunspellPy', 'stem',
+                 (), {'task': 'stem', 'source_fields': {'form'}, 'target_fields': ['anas']})
+hunspell_analyze = ('hunspellpy', 'HunspellPy', 'analyze',
+                    (), {'task': 'analyze', 'source_fields': {'form'}, 'target_fields': ['anas']})
+hunspell_dstem = ('hunspellpy', 'HunspellPy', 'dstem',
+                  (), {'task': 'dstem', 'source_fields': {'form'}, 'target_fields': ['spell', 'anas']})
 
-app = pipeline_rest_api('emMorph', tools, {},  conll_comments=False, singleton_store=singleton_store_factory())
+
+tools = [(hunspell_spell, ('spell',)),
+         (hunspell_stem, ('stem',)),
+         (hunspell_analyze, ('analyze',)),
+         (hunspell_dstem, ('dstem',)),
+         ]
+app = pipeline_rest_api('HunspellPy', tools, {},  conll_comments=False, singleton_store=singleton_store_factory(),
+                        form_title='emMorph demo', form_type='radio',
+                        doc_link='https://github.com/dlt-rilmta/hunspellpy')
 
 if __name__ == '__main__':
     app.run(debug=False)
